375065ab1cdd37d93ad5e1ac4f29a71d4de552d7
briac louazel <briac@laposte.net>
2020-11-13 14:21:54 +0100

    routes

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,21 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+
+    if @dose.save
+      redirect_to @dose
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
 end
